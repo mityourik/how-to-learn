a0f47da69f70ef61ee6b6d3cd5b49cb60411a6c8
Dmitry Orlov <mityourik@gmail.com>
2023-05-05 15:39:40 -0300

    html css js done

--- mesto/blocks/scripts/index.js
+++ mesto/blocks/scripts/index.js
@@ -1,3 +1,4 @@
+// Объявил переменные с постоянным значением
 const buttonPopupOpen = document.querySelector('.profile__popup-open');
 const popup = document.querySelector('.popup');
 const buttonPopupClose = popup.querySelector('.popup__close-button');
@@ -7,30 +8,35 @@ const descriptionField = popup.querySelector('.popup__description-field');
 const profileTitle = document.querySelector('.profile__title');
 const profileParagraph = document.querySelector('.profile__paragraph');
 
+// Функция для добавления/удаления класса у элемента
 const togglePopupState = (popupToToggle) => popupToToggle.classList.toggle('popup__opened');
 
+// Добавил обработчик события на кнопку открытия popup-окна
 buttonPopupOpen.addEventListener('click', () => {
-  togglePopupState(popup);
-  nameField.value = profileTitle.textContent;
-  descriptionField.value = profileParagraph.textContent;
+  togglePopupState(popup); // Открыл popup-окно
+  nameField.value = profileTitle.textContent; // Записал текущее значение заголовка в поле ввода имени/заголовка
+  descriptionField.value = profileParagraph.textContent; // Записал текущее значение описания в поле ввода описания параграфа
 });
 
+// Добавил обработчик события на кнопку закрытия popup-окна
 buttonPopupClose.addEventListener('click', () => {
-  togglePopupState(popup);
-  nameField.value = "";
-  descriptionField.value = "";
+  togglePopupState(popup); // Закрытие popup-окна
+  nameField.value = ""; // Очищение поля ввода имени/заголовка
+  descriptionField.value = ""; // Очищение поле ввода описания параграфа
 });
 
+// Добавил обработчик события на кнопку "сохранить" popup-окна
 buttonPopupSave.addEventListener('click', () => {
-  profileTitle.textContent = nameField.value;
-  profileParagraph.textContent = descriptionField.value;
-  togglePopupState(popup);
+  profileTitle.textContent = nameField.value; // Запись нового значения имени/заголовока
+  profileParagraph.textContent = descriptionField.value; // Запись нового значения описания в параграф
+  togglePopupState(popup); // Закрывание popup-окна
 });
 
+// Добавил обработчик события на popup-окно
 popup.addEventListener('click', (evt) => {
-  if (evt.target === evt.currentTarget) {
-    togglePopupState(popup);
-    nameField.value = "";
-    descriptionField.value = "";
+  if (evt.target === evt.currentTarget) { // Если клик был сделан по фону popup-окна
+    togglePopupState(popup); // Закрывание popup-окна
+    nameField.value = ""; // Очистка поля ввода имени/заголовка
+    descriptionField.value = ""; // Очистка поля ввода описания параграфа
   }
 });
